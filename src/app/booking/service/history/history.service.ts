a257b3441f3612bed8c5974515a624a48152d36b
Asura <u202020239@upc.edu.pe>
2022-11-04 10:25:17 -0500

    feat(history) Added history

--- src/app/booking/service/history/history.service.ts
+++ src/app/booking/service/history/history.service.ts
@@ -1,9 +1,52 @@
 import { Injectable } from '@angular/core';
+import {HttpClient, HttpErrorResponse, HttpHeaders} from "@angular/common/http";
+import {catchError, Observable, retry, throwError} from "rxjs";
 
 @Injectable({
   providedIn: 'root'
 })
 export class HistoryService {
 
-  constructor() { }
+  basePath = ' http://localhost:3000/api/v1/History';
+
+  httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json',
+    })
+  };
+
+  constructor(private http: HttpClient) { }
+
+  handleError(error: HttpErrorResponse) {
+    if (error.error instanceof ErrorEvent) {
+      console.log(`An error occurred: ${error.error.message}`);
+    } else {
+      console.error(`Backend returned code ${error.status}, body was: ${error.error}`);
+    }
+    return throwError(() => new Error('Something happened with request, please try again later'));
+  }
+
+  create(item: any): Observable<History> {
+    return this.http.post<History>(
+        this.basePath,
+        JSON.stringify(item),
+        this.httpOptions)
+        .pipe(
+            retry(2),
+            catchError(this.handleError));
+  }
+
+  getById(id: any): Observable<History> {
+    return this.http.get<History>(
+        `${this.basePath}/${id}`,
+        this.httpOptions)
+        .pipe(
+            retry(2),
+            catchError(this.handleError));
+  }
+
+  getAll(): Observable<History> {
+    return this.http.get<History>(this.basePath, this.httpOptions)
+        .pipe(retry(2), catchError(this.handleError));
+  }
 }
