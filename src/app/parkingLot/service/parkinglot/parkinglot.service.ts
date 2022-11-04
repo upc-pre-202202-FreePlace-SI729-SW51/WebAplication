8b5773a489d17b402fd4e82321eeb939f3f2b51d
Asura <u202020239@upc.edu.pe>
2022-11-04 09:26:22 -0500

    feat(parking lot) Added Parking lot

--- src/app/parkingLot/service/parkinglot/parkinglot.service.ts
+++ src/app/parkingLot/service/parkinglot/parkinglot.service.ts
@@ -1,9 +1,64 @@
 import { Injectable } from '@angular/core';
+import {HttpClient, HttpErrorResponse, HttpHeaders} from "@angular/common/http";
+import {catchError, Observable, retry, throwError} from "rxjs";
+import {Parkinglot} from "../../model/parkinglot";
 
 @Injectable({
   providedIn: 'root'
 })
 export class ParkinglotService {
 
-  constructor() { }
+  basePath = ' http://localhost:3000/api/v1/parkings';
+
+  httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json',
+    })
+  };
+
+  constructor(private http: HttpClient) { }
+
+  handleError(error: HttpErrorResponse) {
+    if (error.error instanceof ErrorEvent) {
+      console.log(`An error occurred: ${error.error.message}`);
+    } else {
+      console.error(`Backend returned code ${error.status}, body was: ${error.error}`);
+    }
+    return throwError(() => new Error('Something happened with request, please try again later'));
+  }
+
+  create(item: any): Observable<Parkinglot> {
+    return this.http.post<Parkinglot>(
+        this.basePath,
+        JSON.stringify(item),
+        this.httpOptions)
+        .pipe(
+            retry(2),
+            catchError(this.handleError));
+  }
+
+  getById(id: any): Observable<Parkinglot> {
+    return this.http.get<Parkinglot>(
+        `${this.basePath}/${id}`,
+        this.httpOptions)
+        .pipe(
+            retry(2),
+            catchError(this.handleError));
+  }
+
+  getAll(): Observable<Parkinglot> {
+    return this.http.get<Parkinglot>(this.basePath, this.httpOptions)
+        .pipe(retry(2), catchError(this.handleError));
+  }
+
+  delete(id: any) {
+    return this.http.delete(`${this.basePath}/${id}`, this.httpOptions)
+        .pipe(retry(2), catchError(this.handleError));
+  }
+
+  update(id: any, item: any): Observable<Parkinglot> {
+    return this.http.put<Parkinglot>(`${this.basePath}/${id}`,
+        JSON.stringify(item), this.httpOptions)
+        .pipe(retry(2), catchError(this.handleError));
+  }
 }
